7c294cbe997b6fad0769423ed46d8347c21be5d5
Anthony N <foto.arn@gmail.com>
2021-01-09 20:15:23 -0800

    + finishing up about me

--- animations/Fullstack.js
+++ animations/Fullstack.js
@@ -1,16 +1,21 @@
 gsap.registerEffect({
   name: "fade",
-  defaults: { duration: 2 }, //defaults get applied to the "config" object passed to the effect below
+  defaults: { duration: 1 }, //defaults get applied to the "config" object passed to the effect below
   effect: (targets, config) => {
-    return gsap.to(targets, { duration: config.duration, opacity: 0 });
+    return gsap.to(targets, { y: 490, duration: 2, ease: Power2.easInout });
   },
 });
 
 //now we can use it like this:
 //gsap.effects.fade(".box");
 
-document.querySelectorAll(".box").forEach(function (box) {
+document.querySelectorAll(".fullstack__img").forEach(function (box) {
   box.addEventListener("mouseenter", function () {
     gsap.effects.fade(this);
   });
 });
+
+const slider = document.querySelector(".slider");
+
+const tl = new TimeLineMax();
+tl.fromTo(slider, 1, { x: "-100%" }, { x: "0%", ease: Power2.easInout }, "-=1");
