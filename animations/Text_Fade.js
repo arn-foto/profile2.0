04e57fb004feef515607396b5cb98ae51012179b
Anthony N <foto.arn@gmail.com>
2021-01-10 00:24:27 -0800

    + About me it good for now

--- animations/Text_Fade.js
+++ animations/Text_Fade.js
@@ -6,7 +6,7 @@ const span = document.querySelector("span");
 
 const tl = new TimelineMax();
 
-tl.fromTo(".nav__left", 1, { x: "-100%" }, { x: "0%", ease: Power2.easInout })
+tl.fromTo(".span", 1, { x: "100%" }, { x: "0%", ease: Power2.easInout })
   .fromTo(
     ".about__me",
     1,
@@ -14,28 +14,5 @@ tl.fromTo(".nav__left", 1, { x: "-100%" }, { x: "0%", ease: Power2.easInout })
     { x: "0%", ease: Power2.easInout },
     "-=1"
   )
-  .fromTo(".stack", 1, { x: "90%" }, { x: "0%", ease: Power2.easInout }, "-=2")
-  .fromTo("h1", 1.5, { x: "90%" }, { x: "0%", ease: Power2.easInout }, "-=1")
-  .fromTo("span", 1, { x: "90%" }, { x: "0%", ease: Power2.easInout }, "-=2");
-
-// gsap.registerEffect({
-//   name: "fade",
-//   defaults: { duration: 1 }, //defaults get applied to the "config" object passed to the effect below
-//   effect: (targets, config) => {
-//     return gsap.to(targets, { y: 490, duration: 2, ease: Power2.easInout });
-//   },
-// });
-
-// //now we can use it like this:
-// //gsap.effects.fade(".box");
-
-// document.querySelectorAll(".fullstack__img").forEach(function (box) {
-//   box.addEventListener("mouseenter", function () {
-//     gsap.effects.fade(this);
-//   });
-// });
-
-// const slider = document.querySelector(".slider");
-
-// const tl = new TimeLineMax();
-// tl.fromTo(slider, 1, { x: "-100%" }, { x: "0%", ease: Power2.easInout }, "-=1");
+  .fromTo(".stack", 2, { x: "-90%" }, { x: "0%", ease: Power2.easInout }, "-=2")
+  .fromTo("h1", 1.5, { x: "90%" }, { x: "0%", ease: Power2.easInout }, "-=1");
