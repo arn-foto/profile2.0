c77e4df2baf44f1596874cdb6f95d7696a7ddaa1
Anthony N <foto.arn@gmail.com>
2021-02-10 15:37:56 -0800

    + Adding links to projects

--- animations/Ball.js
+++ animations/Ball.js
@@ -1,10 +1,10 @@
 var scene = new THREE.Scene();
 document.addEventListener("mousemove", onMouseMove, false);
 var camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
+	75,
+	window.innerWidth / window.innerHeight,
+	0.1,
+	1000
 );
 var mouseX;
 var mouseY;
@@ -14,29 +14,29 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 window.addEventListener("resize", function () {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
 var distance = Math.min(175, window.innerWidth / 4);
 var geometry = new THREE.Geometry();
 
 for (var i = 0; i < 2300; i++) {
-  var vertex = new THREE.Vector3();
+	var vertex = new THREE.Vector3();
 
-  var theta = THREE.Math.randFloatSpread(360);
-  var phi = THREE.Math.randFloatSpread(360);
+	var theta = THREE.Math.randFloatSpread(360);
+	var phi = THREE.Math.randFloatSpread(360);
 
-  vertex.x = distance * Math.sin(theta) * Math.cos(phi);
-  vertex.y = distance * Math.sin(theta) * Math.sin(phi);
-  vertex.z = distance * Math.cos(theta);
+	vertex.x = distance * Math.sin(theta) * Math.cos(phi);
+	vertex.y = distance * Math.sin(theta) * Math.sin(phi);
+	vertex.z = distance * Math.cos(theta);
 
-  geometry.vertices.push(vertex);
+	geometry.vertices.push(vertex);
 }
 var particles = new THREE.Points(
-  geometry,
-  new THREE.PointsMaterial({ color: "cyan", size: 2 })
+	geometry,
+	new THREE.PointsMaterial({ color: "cyan", size: 2 })
 );
 particles.boundingSphere = 50;
 
@@ -50,51 +50,51 @@ scene.add(resizeContainer);
 camera.position.z = 400;
 
 var animate = function () {
-  requestAnimationFrame(animate);
-  renderer.render(scene, camera);
+	requestAnimationFrame(animate);
+	renderer.render(scene, camera);
 };
 
 var myTween;
 function onMouseMove(event) {
-  if (myTween) myTween.kill();
+	if (myTween) myTween.kill();
 
-  mouseX = (event.clientX / window.innerWidth) * 2 - 1;
-  mouseY = -(event.clientY / window.innerHeight) * 2 + 1;
-  myTween = gsap.to(particles.rotation, {
-    duration: 0.1,
-    x: mouseY * -1,
-    y: mouseX,
-  });
-  //particles.rotation.x = mouseY*-1;
-  //particles.rotation.y = mouseX;
+	mouseX = (event.clientX / window.innerWidth) * 2 - 1;
+	mouseY = -(event.clientY / window.innerHeight) * 2 + 1;
+	myTween = gsap.to(particles.rotation, {
+		duration: 0.1,
+		x: mouseY * -1,
+		y: mouseX,
+	});
+	//particles.rotation.x = mouseY*-1;
+	//particles.rotation.y = mouseX;
 }
 animate();
 
 // Scaling animation
 var animProps = { scale: 0.6, xRot: 0, yRot: 0 };
 gsap.to(animProps, {
-  duration: 12,
-  scale: 2.5,
-  repeat: -1,
-  yoyo: true,
-  ease: "sine",
-  onUpdate: function () {
-    renderingParent.scale.set(
-      animProps.scale,
-      animProps.scale,
-      animProps.scale
-    );
-  },
+	duration: 12,
+	scale: 2.5,
+	repeat: -1,
+	yoyo: true,
+	ease: "sine",
+	onUpdate: function () {
+		renderingParent.scale.set(
+			animProps.scale,
+			animProps.scale,
+			animProps.scale
+		);
+	},
 });
 
 gsap.to(animProps, {
-  duration: 120,
-  xRot: Math.PI * 2,
-  yRot: Math.PI * 4,
-  repeat: -1,
-  yoyo: true,
-  ease: "none",
-  onUpdate: function () {
-    renderingParent.rotation.set(animProps.xRot, animProps.yRot, 0);
-  },
+	duration: 120,
+	xRot: Math.PI * 2,
+	yRot: Math.PI * 4,
+	repeat: -1,
+	yoyo: true,
+	ease: "none",
+	onUpdate: function () {
+		renderingParent.rotation.set(animProps.xRot, animProps.yRot, 0);
+	},
 });
